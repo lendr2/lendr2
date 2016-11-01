97da8b7a5b9a3252e344479d5901a0bdebb7485e
Eviscerare <edward.h.guo@gmail.com>
2016-10-31 19:11:34 -0700

    changed routing and setup PSQL hosting with ElephantSQL

--- client/components/Start.js
+++ client/components/Start.js
@@ -6,16 +6,13 @@ class Start extends Component {
   render() {
     return (
       <div className="start-container">
-        <div className="start-contents">
-          <h1>Lendr</h1>
-          <br />
-          <h3>share stuff with your friends.</h3>
-          <br />
-          <br />
-          {this.props.children}
-          <Link to="/login" className="btn btn-primary start-btn">Login</Link>
-          <Link to="/signup" className="btn btn-primary start-btn">Signup</Link>
-        </div>
+        <h1>Lendr</h1>
+        <br/>
+        <h3>share stuff with your friends.</h3>
+        <br/>
+        <br/>
+        <Link to="/login" className="btn btn-primary start-btn">Login</Link>
+        <Link to="/signup" className="btn btn-primary start-btn">Signup</Link>
       </div>
     );
   }
