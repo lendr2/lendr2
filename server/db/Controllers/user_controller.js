b3959bc2daea8b60cbf23c66bb1743b6164a4096
Eviscerare <edward.h.guo@gmail.com>
2016-11-02 14:12:52 -0700

    changes

--- server/db/Controllers/user_controller.js
+++ server/db/Controllers/user_controller.js
@@ -11,61 +11,56 @@ let sessions = sequelize.define('sessions', sessionSchema);
 
 // defines all of the funtions that will be executed on the User table
 let userController = {
-    //creates a user
-    createUser: (req, res, next) => {
-        bcrypt.genSalt(saltRounds, function(err, salt) {
-            bcrypt.hash(req.body.password, salt, null, function(err, hash) {
-                // Store hash in your password DB.
-                req.body.password = hash;
-                res.cookie('ssid', Math.floor(Math.random() * 2132131231) + 1);
-                res.cookie('username', req.body.username);
-                });
-            });
-        sequelize.sync().then( () => {
-             User.create(req.body).then(results => next())
-                                  .catch( error => res.status(400).end());
-          }).catch( () => res.status(400).send('error'));
+  //creates a user
+  createUser: (req, res, next) => {
+    req.body.password = bcrypt.hashSync(req.body.password, saltRounds);
+    res.cookie('ssid', Math.floor(Math.random() * 2132131231) + 1);
+    res.cookie('username', req.body.username);
+    sequelize.sync().then(() => {
+      User.create(req.body).then(results => next())
+        .catch(error => res.status(400).end());
+    }).catch(() => res.status(400).send('error'));
+  },
 
-},
+  //gets a user for validation on login
+  getUser: (req, res, next) => {
+    sessions.find({
+      where: {
+        ssid: req.body.ssid
+      }
+    }).then((user) => {
+      //user truthy
+      if (user) {
+        res.cookie('username', user.username);
 
-    //gets a user for validation on login
-    getUser: (req, res, next) => {
-        sessions.find({
-            where: {
-                ssid: req.body.ssid
-            }
-        }).then((user) => {
-            //user truthy
-            if (user) {
-                res.cookie('username', user.username);
-
-            }
-            //user falsy
-            if (!user) {
-                User.findOne({
-                    where: {
-                        username: req.body.username
-                    }
-                }).then( user => {
-                    if (!user) res.status(400).send('no user is the db')
-                    if (user){bcrypt.compare(req.body.password, user.dataValues.password, function(err, val) {
-                        if (err || val === false)
-                            res.status(400).send('incorrect username or password.');
-                        req.session.user = user.dataValues.username;
-                        res.cookie('username', user.username);
-                        req.session.save(() => console.log('saving session'));
-                        next();
-                    })
-                }
-                }).catch(err => {
-                    console.log(err);
-                    res.status(400).end();
-                })
-            }
-        }).catch((err) => {
-            res.status(400).send(err);
+      }
+      //user falsy
+      if (!user) {
+        User.findOne({
+          where: {
+            username: req.body.username
+          }
+        }).then(user => {
+          if (!user) res.status(400).send('no user is the db')
+          if (user) {
+            bcrypt.compare(req.body.password, user.dataValues.password, function (err, val) {
+              if (err || val === false)
+                res.status(400).send('incorrect username or password.');
+              req.session.user = user.dataValues.username;
+              res.cookie('username', user.username);
+              req.session.save(() => console.log('saving session'));
+              next();
+            })
+          }
+        }).catch(err => {
+          console.log(err);
+          res.status(400).end();
         })
-    }
+      }
+    }).catch((err) => {
+      res.status(400).send(err);
+    })
+  }
 
 }
 
